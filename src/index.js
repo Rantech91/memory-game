80c54348325d76f0dbd41652dcfdea8263247ceb
Jose Recart <joserecart91@gmail.com>
2022-09-20 20:59:05 -0300

    coding part1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener('DOMContentLoaded', () => {
+    // card options
+    const cards = [
+        {
+            name: 'fries',
+            img: 'src/images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'src/images/cheeseburger.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'src/images/ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'src/images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'src/images/milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'src/images/hotdog.png'
+        },
+        {
+            name: 'fries',
+            img: 'src/images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'src/images/cheeseburger.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'src/images/ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'src/images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'src/images/milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'src/images/hotdog.png'
+        },
+    ]
+    
+    cards.sort(() => 0.5 - Math.random())
+    console.log(cards)
+
+    const grid = document.querySelector('.grid')
+
+    function createBoard(){
+        for 
+    }
+})
