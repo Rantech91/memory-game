5c34353013a35baf1f73608f066beddb9237f276
Jose Recart <joserecart91@gmail.com>
2022-09-20 21:30:01 -0300

    codinggg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // card options
-    const cards = [
+    const cardArray = [
         {
             name: 'fries',
             img: 'src/images/fries.png'
@@ -51,12 +51,44 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     ]
     
-    cards.sort(() => 0.5 - Math.random())
-    console.log(cards)
+    cardArray.sort(() => 0.5 - Math.random())
+    console.log(cardArray)
 
     const grid = document.querySelector('.grid')
+    let cardsChosen = []
+    let cardsChosenIds = []
 
     function createBoard(){
-        for 
+        for (let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement('img')
+            card.setAttribute('src', 'src/images/blank.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+    }}
+    createBoard()
+
+    function flipCard(){
+        let cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenIds.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
     }
+    function checkForMatch(){
+        const cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenIds[0]
+        const optionTwoId = cardsChosenIds[1]
+
+        if (optionOneId == optionTwoId) {
+            alert('You have clicked the same image!')
+            cards[optionOneId].setAttribute('src', 'src/images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'src/images/blank.png')
+        } else if (){
+
+        }
+    }
+    createBoard()
 })
